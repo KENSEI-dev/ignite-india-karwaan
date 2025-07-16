c00e237e055d2d563694eaceb126004bdec8ebf8
KENSEI <107671402+KENSEI-dev@users.noreply.github.com>
2025-07-16 11:44:17 +0530

    Update py_ass.py

--- py_ass.py
+++ py_ass.py
@@ -6,3 +6,28 @@ for i in range(n):
 for item in arr:
    print(item,end=' ')
 print()
+
+Q1
+
+arr = [10, 20, 30, 40]
+print("enter array elements")
+for item in arr:
+    print(item, end=' ')
+print()
+print("The individual elements are:")
+print("Element at index 0:", arr[0])
+print("Element at index 1:", arr[1])
+print("Element at index 2:", arr[2])
+print("Element at index 3:", arr[3])
+
+Q2
+
+arr = [10, 20, 30, 40]
+n=int(input("enter an element to be added in the end of the array:"))
+arr.append(n)
+print("new array:")
+for item in arr:
+    print(item, end=' ')
+print()
+
+Q3
